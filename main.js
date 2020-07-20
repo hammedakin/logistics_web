cb88489657672633444ce4bf59d89df7aa1b8a32
omoluabii <hakra911@gmail.com>
2020-07-20 15:27:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// Responsive Nav
+
+// Smooth Scrolling
+$('.topnav a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
